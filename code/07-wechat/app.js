ae0c47108d30b3605a0e2483cb7148a4330011d3
puli <123@123.com>
2020-09-17 01:10:03 +0800

    登录

--- code/07-wechat/app.js
+++ code/07-wechat/app.js
@@ -4,6 +4,9 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+//记录所有已经登录过的用户,防止登陆两个名称一样的
+const users = []
+
 server.listen(3000,() => {
     console.log("服务器启动成功了")
 })
@@ -12,18 +15,24 @@ server.listen(3000,() => {
 //把public目录设置为静态资源目录就是'/'
 app.use(require('express').static('public'))
 
-
+//访问/首页的时候
 app.get('/',function (req,res) {
     // res.sendFile(__dirname+'/index.html')
+    
     res.redirect('/index.html');//重定向到/index.html
 })
 
 io.on('connection',function (socket) {
-    socket.emit('news',{hello:'World'})
-    socket.on('my other event',function (data) {
-        console.log(data)
-    })
-    socket.on('hehe',function (data) {
-        console.log(data)
+    socket.on('login',data => {
+        //判断此用户名是否已登录
+        let user = users.find(item => item.username === data.username)
+        if(user){
+            socket.emit('loginError',{msg:'用户名已经存在，请换个名字'})
+            console.log("登陆失败")
+        }else{
+            users.push(data)
+            socket.emit('loginSuccess',data)
+            console.log("登陆成功")
+        }
     })
 })
--- code/07-wechat/public/js/index.js
+++ code/07-wechat/public/js/index.js
@@ -3,3 +3,46 @@
 
 //1.连接socket服务
 var socket = io('http://localhost:3000')
+
+
+//2.登陆功能
+//siblings() 方法返回被选元素的所有同级元素。
+$('#login_avatar li').on('click',function() {
+    $(this).addClass('now').siblings().removeClass('now')
+})
+
+//3.点击登录按钮 登录
+$('#loginBtn').on('click',function(){
+    //3.1获取用户名
+    var username = $('#username').val().trim()
+    //判断有无输入
+    if(!username){
+        alert('请输入用户名')
+        return
+    }
+
+    //3.2获取用户选择的头像
+    var avatar = $('#login_avatar li.now img').attr('src')
+    if(!avatar){
+        alert('请选择一个头像')
+        return
+    }
+
+    //3.3把用户名，头像告诉socket io服务器，登陆了
+    socket.emit('login',{username:username,avatar:avatar})
+
+})
+
+
+
+
+
+
+
+//监听登录失败的请求
+socket.on('loginError',data => {alert('登陆失败')})
+//监听登录成功的请求
+socket.on('loginSuccess',data => {alert('登陆成功')})
+
+
+
