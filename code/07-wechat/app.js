6e57679f3ee5a327696243eb74635d30437c37a9
puli <123@123.com>
2020-09-18 02:04:55 +0800

    wechat

--- code/07-wechat/app.js
+++ code/07-wechat/app.js
@@ -21,7 +21,7 @@ app.get('/',function (req,res) {
     
     res.redirect('/index.html');//重定向到/index.html
 })
-
+//socket每个用户都有一个，所以页面不能刷新，一刷新就断了，相当于重新连接
 io.on('connection',function (socket) {
     socket.on('login',data => {
         //判断此用户名是否已登录
@@ -30,9 +30,55 @@ io.on('connection',function (socket) {
             socket.emit('loginError',{msg:'用户名已经存在，请换个名字'})
             console.log("登陆失败")
         }else{
-            users.push(data)
+            users.push(data)//填入数组
+            //1.告诉客户端登陆成功
             socket.emit('loginSuccess',data)
-            console.log("登陆成功")
+            // console.log(data.username+"：登陆成功")
+
+            //2.告诉所有的用户，有用户加入了聊天室
+            //socket.emit:告诉当前用户； io.emit：告诉所有用户
+            io.emit('addUser',data)
+
+            //3.告诉所有的用户，目前聊天室有多少人
+            io.emit('userList',users)
+
+            //把登陆成功的用户名和头像存储起来
+            socket.username = data.username
+            socket.avatar = data.avatar
+
+
         }
     })
+
+    //固定的用户断开监听
+    socket.on('disconnect',() => {
+        //1.从用户组中删除用户
+        let idx = users.findIndex(item => item.username === socket.username)
+        users.splice(idx,1)//删除对应下表的用户
+        //2.告诉所有用户某某退出
+        io.emit('delUser',{
+            username:socket.username,
+            avatar:socket.avatar
+        })
+        //3.告诉所有人更新userList
+        io.emit('userList',users)
+    })
+
+    //监听用户发来的消息
+    socket.on('sendMsg',data => {
+        // console.log(data)
+        //1.广播给所有的用户
+        io.emit('receiveMsg',data)
+    })
+
+ //监听用户发来的图片
+ socket.on('sendImage',data => {
+    // console.log(data)
+    //1.广播给所有的用户
+    io.emit('receiveImage',data)
+})
+
+
+
+
 })
--- code/07-wechat/public/js/index.js
+++ code/07-wechat/public/js/index.js
@@ -3,7 +3,7 @@
 
 //1.连接socket服务
 var socket = io('http://localhost:3000')
-
+var username,avatar
 
 //2.登陆功能
 //siblings() 方法返回被选元素的所有同级元素。
@@ -33,16 +33,208 @@ $('#loginBtn').on('click',function(){
 
 })
 
+//4.聊天功能
+$('.btn-send').on('click',() =>{
+    //4.1获取聊天内
+    var content = $('#content').val().trim()
+    console.log("content:"+content)
+    $('#content').val('')//置空
+    //判断有无输入
+    if(!content){
+        return alert('请输入内容')
+    }
+
+
+    //4.2发送给服务器    
+    socket.emit('sendMsg',{msg:content,username:username,avatar:avatar})
+
+})
+
+//5.scrollIntoView
+function scrollIntoView(){
+    //获取$('.box-bd')的最后一个子元素，让其地部一直可以看到
+    $('.box-bd').children(':last').get(0).scrollIntoView(false)
+}
+
+//6.发送图片功能
+$('#file').on('change',function(){
+    var file = this.files[0]//拿到文件
+    //把这个文件发送到服务器,借助H5新增的fileReader
+    var fr = new FileReader()
+    fr.readAsDataURL(file)
+    fr.onload=function(){
+       socket.emit('sendImage',{
+           username:username,
+           avatar:avatar,
+           img:fr.result
+       })
+    }
+})
+
+//7.点击时，初始化jquery-emoji插件
+$('.face').on('click',function(){
+    $('#content').emoji({
+        button:'.face',//出发表情的元素，如不指定，会自动生成一个
+        showTab: false,//
+        animation: 'slide',
+        position: 'topRight',
+        icons: [{
+            name: "QQ表情",
+            path: "lib/jquery-emoji/img/qq/",
+            maxNum: 91,
+            excludeNums: [41, 45, 54],
+            file: ".gif"
+        }]
+    })
+})
 
 
 
 
+/* ====================== 监听事件 ======================================= */
 
-
-//监听登录失败的请求
+//1.监听登录失败的请求
 socket.on('loginError',data => {alert('登陆失败')})
-//监听登录成功的请求
-socket.on('loginSuccess',data => {alert('登陆成功')})
+//2.监听登录成功的请求
+socket.on('loginSuccess',data => {
+    console.log(data)
+    //2.1隐藏登陆窗口，显示聊天窗口
+    $('.login_box').fadeOut()
+    $('.container').fadeIn(20,"linear")
+    //2.2设置个人信息
+    $('.avatar_url').attr('src',data.avatar)
+    $('.username').text(data.username)
+    //存储用户名头像
+    username = data.username
+    avatar = data.avatar
+  
+})
+
+//3.监听添加用户的请求
+socket.on('addUser',data => {
+    //添加一条系统消息
+    $('.box-bd').append(`
+        <div class="system">
+            <p class="message_system">
+                <span class="content"><i><b>${data.username}</b></i> 加入群聊</span>
+            </p>
+        </div>
+    `)
+})
+
+
+//4.监听用户列表的消息
+socket.on('userList',data => {
+    //把userList中的数据动态渲染到左侧菜单中
+    $('.user-list ul').html('')
+    $('#userCount').text(data.length)
+    data.forEach(item => {
+        $('.user-list ul').append(`
+            <li class="user">
+                <div class="avatar"><img src="${item.avatar}" alt=""></div>
+                <div class="name">${item.username}</div>
+            </li>
+        `)
+        scrollIntoView()
+    })
+    
+})
+
+
+//5.监听用户离开的消息
+socket.on('delUser',data => {
+    //添加一条系统消息
+    $('.box-bd').append(`
+        <div class="system">
+            <p class="message_system">
+                <span class="content"><i><b>${data.username}</b></i> 离开群聊</span>
+            </p>
+        </div>
+    `)
+
+    scrollIntoView()
+})
+
+ //6.监听服务器发来的用户的消息
+ socket.on('receiveMsg',data => {
+    console.log(data)
+    //1.把接收到的消息加入到聊天窗口中
+    if(data.username === username){
+        //自己的消息
+        $('.box-bd').append(`
+        <div class="message-box">
+            <div class="my message">
+                <img src="${data.avatar}" alt="" class="avatar">
+                <div class="content">
+                    <div class="bubble">
+                        <div class="bubble_cont">${data.msg}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `)
+    }else{
+        //别人的消息
+        $('.box-bd').append(`
+        <div class="message-box">
+            <div class="other message"></div>
+                <img src="${data.avatar}" alt="" class="avatar">
+                <div class="nickname">${data.username}</div>
+                <div class="content">
+                    <div class="bubble">
+                        <div class="bubble_cont">${data.msg}</div>            
+                    </div>
+                </div>
+            </div>
+        </div>
+    `)
+    }
+    //获取$('.box-bd')的最后一个子元素，让其地部一直可以看到
+    scrollIntoView()
+})
+
+//6.监听服务器发来的用户的图片
+socket.on('receiveImage',data => {
+    console.log(data)
+    //1.把接收到的消息加入到聊天窗口中
+    if(data.username === username){
+        //自己的消息
+        $('.box-bd').append(`
+        <div class="message-box">
+            <div class="my message">
+                <img src="${data.avatar}" alt="" class="avatar">
+                <div class="content">
+                    <div class="bubble">
+                        <img src="${data.img}">                       
+                    </div>
+                </div>
+            </div>
+        </div>
+    `)
+    }else{
+        //别人的消息
+        $('.box-bd').append(`
+        <div class="message-box">
+            <div class="other message"></div>
+                <img src="${data.avatar}" alt="" class="avatar">
+                <div class="nickname">${data.username}</div>
+                <div class="content">
+                    <div class="bubble">
+                     <img src="${data.img}">
+                    </div>
+                </div>
+            </div>
+        </div>
+    `)
+    }
+    //等待图片加载完成,再滚动到底部
+    $('.box-bd img:last').on('load',function(){
+        //获取$('.box-bd')的最后一个子元素，让其地部一直可以看到
+        scrollIntoView()
+    })
+    
+    
+})
 
 
 
